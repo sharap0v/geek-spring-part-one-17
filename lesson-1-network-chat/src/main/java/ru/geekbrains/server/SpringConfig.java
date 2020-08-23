375a09db4908cf29732964e8ead6f8ececfc6368
Sergey Zelentsov <_sharapov_@mail.ru>
2020-08-23 21:31:08 +0300

    hw1

---
+++ lesson-1-network-chat/src/main/java/ru/geekbrains/server/SpringConfig.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import ru.geekbrains.server.auth.AuthService;
+import ru.geekbrains.server.auth.AuthServiceJdbcImpl;
+import ru.geekbrains.server.persistance.UserRepository;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@ComponentScan(basePackages = "ru.geekbrains.server")
+public class SpringConfig {
+    @Bean
+    public ChatServer chatServer(AuthService authService){
+        return new ChatServer(authService);
+    }
+    @Bean
+    public AuthService authService(UserRepository userRepository){
+        return new AuthServiceJdbcImpl(userRepository);
+    }
+    @Bean
+    public UserRepository userRepository(DataSource dataSource) throws SQLException {
+        return new UserRepository(dataSource);
+    }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        ds.setUrl("jdbc:mysql://localhost:3306/network_chat?&amp;useUnicode=true&amp;characterEncoding=UTF-8&amp;serverTimezone=UTC");
+        return ds;
+    }
+}
